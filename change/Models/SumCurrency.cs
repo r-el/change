01a2f60077abf5e255bf578ca008d489579404c3
rel <rel@mail.com>
2021-11-22 00:09:56 +0200

    Add SumCurrency Class

---
+++ change/Models/SumCurrency.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace change.Models
+{
+    // סך כל המטבעות
+    public class SumCurrency
+    {
+        public SumCurrency() { DateTime = DateTime.Now; }
+
+        [Key]
+        public int ID { get; set; }
+
+        [Timestamp]
+        public byte[] DateTimeInDB { get; set; } // תאריך מבסיס הנתונים
+
+        public DateTime DateTime { get; set; }   // תאריך
+
+        public Currency Currency { get; set; }   // מטבע
+
+        public CashRegister Cash { get; set; }   // קופה
+
+        [Display(Name = "סכום")]
+        public decimal Sum { get; set; }
+    }
+}

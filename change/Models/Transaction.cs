e82c87e365dcabf1d3708c72a25d9212c7ebb28f
rel <rel@mail.com>
2021-11-21 23:23:57 +0200

    Add Transaction Class

---
+++ change/Models/Transaction.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace change.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Timestamp]
+        public byte[] DateTimeInDB { get; set; }
+
+        public DateTime DateTime { get { return DateTime.FromBinary(BitConverter.ToInt64(DateTimeInDB, 0)); } }
+
+        public CashRegister CashRegister { get; set; }
+
+        public Customer Customer { get; set; }
+
+        public CurrencyValueByDate Rate { get; set; }
+
+        public bool IsBuying { get; set; } // הקונה נותן שקלים וקונה דולרים
+
+        [Display(Name = "שקלים")]
+        public decimal NIS { get; set; }
+        [Display(Name = "מטבע")]
+        public decimal Coin { get; set; }
+
+        // חשב את העסקה לאחר עמלה
+        public decimal Calculate
+        {
+            set
+            {
+                if (IsBuying)
+                    NIS = Coin * decimal.Parse((Rate.Rate * 1.01).ToString());
+                else
+                    NIS = Coin * decimal.Parse((Rate.Rate * 0.99).ToString());
+            }
+        }
+    }
+}

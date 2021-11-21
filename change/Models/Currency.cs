0d0d54cd322ee83904b45f4b9fcf2ff383cfb27c
rel <rel@mail.com>
2021-11-21 22:02:14 +0200

    Add Currency Class

---
+++ change/Models/Currency.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace change.Models
+{
+    public class Currency
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Display(Name = "שם מטבע")]
+        public string Name { get; set; }
+
+        [Display(Name = "קוד מטבע")]
+        public string CurrencyCode { get; set; }
+
+        [Display(Name = "מדינה")]
+        public string Country { get; set; }
+
+        [Display(Name = "שם מטבע ומדינה")]
+        public string NameAndCountry { get { return Name + " " + Country; } }
+    }
+}

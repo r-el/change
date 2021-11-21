4efd42c352a3548407f3d05599cdf6664ab83d59
rel <rel@mail.com>
2021-11-21 22:16:49 +0200

    Add CurrencyValueByDate Class

---
+++ change/Models/CurrencyValueByDate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace change.Models
+{
+    public class CurrencyValueByDate
+    {
+        [Key]
+        public int ID { get; set; }
+
+        // תאריך עדכון
+        public DateTime Last_Date { get; set; } = new DateTime();
+
+        // סוג מטבע
+        public Currency Currency { get; set; }
+
+        [Display(Name = "יחידה")]
+        public int Unit { get; set; }
+
+        // ערך בשקלים
+        public double Rate { get; set; }
+
+        // הוספת/הפחתת אחוזים לשער החליפין
+        public double BuyRate { get { return Rate * 1.01; } }  // כשבא קונא למכור
+        public double SellRate { get { return Rate * 0.99; } } // כשבא קונה לקנות
+    }
+}
